6ff7a78be7ebd7d93523a9dbeaf609c0ca7d195a
AnonymousHoax <108090689+ManongGuardVpnV2@users.noreply.github.com>
2026-01-14 19:55:31 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 // Initialize database client
-const supabaseUrl = 'https://jboemtbxqztibokysvjl.databasepad.com';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjYwNDQwYzJhLTcxMDQtNDU2ZC1iNDdhLTgwYTAwMDE4ZDVlYiJ9.eyJwcm9qZWN0SWQiOiJqYm9lbXRieHF6dGlib2t5c3ZqbCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzY4MzU3NzAwLCJleHAiOjIwODM3MTc3MDAsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.v-7Ukb6ocjjCIYvR7PsxiVtNR2oTF4wrWyMUhK5n37A';
+const supabaseUrl = 'https://rgjbdalouaneehrknqqs.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnamJkYWxvdWFuZWVocmtucXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzODM2NjEsImV4cCI6MjA4Mzk1OTY2MX0.0mifRlDqheeVsWo78KBu0oSruZoMCmdRRTVs4epy43w';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
