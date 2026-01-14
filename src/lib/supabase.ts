022799432867b8d53db29e2798cb9136c4ba16bd
AnonymousHoax <108090689+ManongGuardVpnV2@users.noreply.github.com>
2026-01-14 20:23:11 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -8,3 +8,12 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 export { supabase };
+
+
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
